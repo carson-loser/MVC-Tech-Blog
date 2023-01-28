592eae9f392c6a78e6ab7814e3d7ae337dc9d529
carson-loser <carson.loser@gmail.com>
2023-01-27 18:03:29 -0600

    added file structure and set up server file

---
+++ server.js
@@ -0,0 +1,18 @@
+// requiring express
+const express = require('express');
+
+// initializing express
+const app = express();
+// setting port for Heroku and default port 3001
+const PORT = process.env.PORT || 3001;
+
+// initializing sequelize through config folder and file
+const sequelize = require('./config/config');
+
+
+// setting up port and syncing database
+app.listen(PORT, () => {
+  console.log(`Listening on port: ${PORT}!`);
+  // dont reset database with changes made
+  sequelize.sync({ force: false });
+})
